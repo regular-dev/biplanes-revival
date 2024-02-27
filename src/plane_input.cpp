9eb02aa3bfe92c462b3963dbc9f98627c956590d
casqade <ivamspasibo@gmail.com>
2024-02-27 18:12:50 +0300

    fix: plane_input.cpp code style

--- src/plane_input.cpp
+++ src/plane_input.cpp
@@ -29,17 +29,20 @@ Plane::Input::setPlane(
   plane = parentPlane;
 }
 
-void Plane::Input::Accelerate()
+void
+Plane::Input::Accelerate()
 {
   plane->Accelerate();
 }
 
-void Plane::Input::Decelerate()
+void
+Plane::Input::Decelerate()
 {
   plane->Decelerate();
 }
 
-void Plane::Input::TurnLeft()
+void
+Plane::Input::TurnLeft()
 {
   if ( plane->mHasJumped == true )
     plane->pilot.Move(PLANE_PITCH::PITCH_LEFT);
@@ -48,7 +51,8 @@ void Plane::Input::TurnLeft()
     plane->Turn(PLANE_PITCH::PITCH_LEFT);
 }
 
-void Plane::Input::TurnRight()
+void
+Plane::Input::TurnRight()
 {
   if ( plane->mHasJumped == true )
     plane->pilot.Move(PLANE_PITCH::PITCH_RIGHT);
@@ -57,18 +61,21 @@ void Plane::Input::TurnRight()
     plane->Turn(PLANE_PITCH::PITCH_RIGHT);
 }
 
-void Plane::Input::TurnIdle()
+void
+Plane::Input::TurnIdle()
 {
   if ( plane->mHasJumped == true )
     plane->pilot.MoveIdle();
 }
 
-void Plane::Input::Shoot()
+void
+Plane::Input::Shoot()
 {
   plane->Shoot();
 }
 
-void Plane::Input::Jump()
+void
+Plane::Input::Jump()
 {
   if ( plane->mHasJumped == true )
     plane->pilot.OpenChute();
